36ea4ebe2582282f0eefe630d3a27baf221937a2
Federico Allegrini <federicoa@mondoconvenienza.net>
2020-08-25 17:08:09 +0200

    Core concept - routing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Do work here
-router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+router.get("/", (req, res) => {
+  // Send back stuff to client
+  // res.send("Hey! It works!");
+  // const jsonObj = { name: "Fede", age: 25, cool: true };
+  // res.json(jsonObj);
+
+  // Query params
+  // res.send(req.query.name);
+  res.json(req.query);
+
+  // Post params
+  // res.send(req.body);
+});
+
+router.get("/reverse/:name/:last", (req, res) => {
+  const reverse = [...req.params.name].reverse().join("");
+  res.send(reverse + " - " + req.params.last);
 });
 
 module.exports = router;
