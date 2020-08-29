6990185c944ab476cfb660a757834bcae2140a10
Federico Allegrini <federico.allegrini.dev@gmail.com>
2020-08-29 23:55:09 +0200

    Core concept - middleware and error handling

--- app.js
+++ app.js
@@ -72,6 +72,7 @@ app.use((req, res, next) => {
 // After allllll that above middleware, we finally handle our own routes!
 app.use("/", routes);
 
+// Use of Error Handlers
 // If that above routes didnt work, we 404 them and forward to error handler
 app.use(errorHandlers.notFound);
 
--- controllers/storeController.js
+++ controllers/storeController.js
@@ -3,6 +3,20 @@
 // View SHOWS data extracted
 // Controller ORGANIZES all operation between Model and View
 
+exports.myMiddleware = (req, res, next) => {
+  req.name = "Fede";
+
+  // Cookie parser is an example of Middleware
+  // res.cookie("name", "Fede is cool", { maxAge: 90000000 });
+
+  // Throw an error for see the Error Handler in action
+  if (req.name === "Fede") {
+    throw Error("That is stupid name");
+  }
+  next();
+};
+
 exports.homePage = (req, res) => {
+  console.log("[storeController - homePage] " + req.name);
   res.render("index");
 };
