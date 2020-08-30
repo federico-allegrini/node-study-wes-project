6d41d0ca665cd36cfe57dbea4f9de92b59c4dd17
Federico Allegrini <federico.allegrini.dev@gmail.com>
2020-08-30 13:14:57 +0200

    Cleaning up the middleware, creating our store model

---
+++ models/Store.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+const slug = require("slugs");
+
+const storeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true, // Auto trim before store into DB
+    required: "Please enter a store name!", // If the field isn't passed return this error message
+  },
+  slug: String,
+  description: {
+    type: String,
+    trim: true,
+  },
+  tags: [String],
+});
+
+// Every single time we save a Store, will be executed this code
+// Generate slug from name using the slug package
+storeSchema.pre("save", function (next) {
+  if (!this.isModified("name")) {
+    next(); // Skip it
+    return; // Stop this function from running
+  }
+  this.slug = slug(this.name);
+  next();
+  // TODO make more resilient so slug are unique
+});
+
+module.exports = mongoose.model("Store", storeSchema);
