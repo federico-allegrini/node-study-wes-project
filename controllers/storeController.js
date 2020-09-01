e2a7303c2520d2baabf4b0a3bcb47628be32d43d
Federico Allegrini <federicoa@mondoconvenienza.net>
2020-09-01 10:02:31 +0200

    Saving stores to DB with async await

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,6 @@
+const mongoose = require("mongoose");
+const Store = mongoose.model("Store");
+
 exports.homePage = (req, res) => {
   console.log("[storeController - homePage] " + req.name);
   res.render("index");
@@ -7,6 +10,11 @@ exports.addStore = (req, res) => {
   res.render("editStore", { title: "Add Store" });
 };
 
-exports.createStore = (req, res) => {
-  res.json(req.body);
+// The "async" keyword tell to browser or server that the function can contains "await" operations
+exports.createStore = async (req, res) => {
+  const store = new Store(req.body);
+  // Use "await" keyrord for run asynchronous code, like synchronous one
+  // Await until the asynchronous code is finish, then pass to next line
+  await store.save();
+  res.redirect("/");
 };
