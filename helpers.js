a04773793c2088c4f3701ac70392c135a36ef066
Federico Allegrini <federicoa@mondoconvenienza.net>
2020-09-09 12:47:30 +0200

    Routing and templating single stores

--- helpers.js
+++ helpers.js
@@ -13,7 +13,7 @@ exports.dump = (obj) => JSON.stringify(obj, null, 2);
 
 // Making a static map is really long - this is a handy helper function to make one
 exports.staticMap = ([lng, lat]) =>
-  `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=800x150&key=${process.env.MAP_KEY}&markers=${lat},${lng}&scale=2`;
+  `https://image.maps.ls.hereapi.com/mia/1.6/mapview?apiKey=${process.env.MAP_HERE_KEY}&lat=${lat}&lon=${lng}&z=13&w=800&h=150&c=${lat},${lng}&style=fleet`;
 
 // Inserting an SVG
 exports.icon = (name) => fs.readFileSync(`./public/images/icons/${name}.svg`);
